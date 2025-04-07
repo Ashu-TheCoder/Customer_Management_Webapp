1be9d08ade795ee30e612e0f6109a1f191afbc8e
Ashish Bonde <ashishbonde8484@gmail.com>
2025-04-07 12:32:17 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -146,4 +146,4 @@ elif st.session_state['DB_Field']:
 # Footer
 
 st.write("---")
-st.markdown(f"**Developed by Ashish Bonde** <br> [LinkedIn](www.linkedin.com/in/ashish-bonde)<br> [GitHub Profile](https://github.com/Ashu-TheCoder)", unsafe_allow_html=True)
+st.markdown(f"**Developed by Ashish Bonde** <br> [LinkedIn](https://www.linkedin.com/in/ashish-bonde/)<br> [GitHub Profile](https://github.com/Ashu-TheCoder)", unsafe_allow_html=True)
